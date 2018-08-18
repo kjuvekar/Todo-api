20335fa4cb2c6e70cbca2e8ea6ed02e9c6f092e5
kjuvekar <kalpeshjuvekar96@gmail.com>
2018-08-19 00:11:15 +0530

    delete commands deletemany deleteone findOneAndDelete

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -6,8 +6,14 @@ MongoClient.connect('mongodb://localhost:27017/Todo',{ useNewUrlParser: true },(
     }
     console.log('Connected to DB!');
     var dbo = db.db('TodoApp');
-    dbo.collection('Todos').deleteMany({completed: true}).then((res)=>{
+    // dbo.collection('Todos').deleteMany({completed: true}).then((res)=>{
+    //     console.log(res);
+    // });
+    // dbo.collection('Users').deleteOne({age:22}).then((res)=>{
+    //     console.log(res);
+    // })    ;
+    dbo.collection('Users').findOneAndDelete({age:22}).then((res)=>{
         console.log(res);
-    });    
+    });
     db.close();
 });
