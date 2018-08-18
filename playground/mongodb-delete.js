f2608e8a83f9bfa2e88879c33b59d24bbf60d551
kjuvekar <kalpeshjuvekar96@gmail.com>
2018-08-18 23:48:48 +0530

    delete command

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,13 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/Todo',{ useNewUrlParser: true },(err,db) => {
+    if(err){
+       return console.log('Unable to connect to the database!');
+    }
+    console.log('Connected to DB!');
+    var dbo = db.db('TodoApp');
+    dbo.collection('Todos').deleteMany({completed: true}).then((res)=>{
+        console.log(res);
+    });    
+    db.close();
+});
