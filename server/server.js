2eb1dbf5b2c1909f3cdb4dd41752e66b9de60fd2
kjuvekar <kalpeshjuvekar96@gmail.com>
2018-08-31 23:22:16 +0530

    new users collection

--- server/server.js
+++ server/server.js
@@ -3,36 +3,57 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 var db = mongoose.connect('mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true });
 
-var Todo = mongoose.model('Todo',{
-    text:{
-        type:String
+// var Todo = mongoose.model('Todo',{
+//     text:{
+//         type:String,
+//         required:true,
+//         minlength:1,
+//         trim:true
+//     },
+//     completed:{
+//         type:Boolean,
+//         default:false
+//     },
+//     completedAt:{
+//         type:Date,
+//         default:null
+//     }
+// });
+//      var todo = new Todo({
+//         text:'Cook rice',
+//         completed:false
+//      })
+//     todo.save()
+//         .then((res)=>{
+//             console.log(res);
+//             mongoose.connection.close();
+//         }).catch((err)=>{
+//             console.log('Error!',err);
+//             mongoose.connection.close();
+//         });
+
+var user = mongoose.model('users',{
+    name:{
+        type:string,
+        required:true,
+        minlength:1,
+        trim:true
     },
-    completed:{
-        type:Boolean
-    },
-    completedAt:{
-        type:Date,
-        default:Date.now()
+    email:{
+        type:string,
+        required:true,
+        trim:true,
+        minlength:1
     }
 });
-
-// var newTodo = new Todo({
-//     text:'Cook it!'
-// });
-     var todonew = new Todo({
-        text:'new todo task2',
-        completed:true
-     })
-// newTodo.save()
-//     .then((res)=>{
-//     console.log(res);})
-//     .catch((err=>{
-//     console.log(err);}))
-todonew.save().then((res)=>{
-    console.log(res);
-}).catch((err)=>{
-    console.log('Error!',err);
-});
-
-// db.close();
-mongoose.connection.close();
+    var newUser = new user({
+        name:'Kalpesh  juvekar  ',
+        email:'kalpeshjuvekar96@gmail.com'
+    })
+    newUser.save().thrn((res)=>{
+        console.log(res);
+        mongoose.connection.close();
+    }).catch((err)=>{
+        console.log('Sorry problem occured!',err);
+        mongoose.connection.close();
+    });
