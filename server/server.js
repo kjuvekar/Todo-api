df66901334cfafac263e84dee5787995d3974fb4
kjuvekar <kalpeshjuvekar96@gmail.com>
2018-09-01 09:52:18 +0530

    route created for post todo

--- server/server.js
+++ server/server.js
@@ -5,3 +5,25 @@ var {mongoose} = require('./db/mongodbConfig');
 var {user} = require('./model/user');
 var {todo} = require('./model/todo');
 
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todo',(req,res)=>{
+    var todonew = new todo({
+        text : req.body.text,
+        completed: req.body.completed
+    });
+    todonew.save().then((doc)=>{
+        console.log(doc);
+        res.send(doc);
+    }).catch((err)=>{
+        console.log(err);
+        res.status(400).send(err);
+    });
+    console.log(req.body);
+});
+
+app.listen(3000,()=>{
+    console.log('started on port 3000');
+});
